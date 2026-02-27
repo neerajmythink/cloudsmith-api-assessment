5836d2974abbe4bca4e44bc2bda04bb03154bb26
Neeraj.Srivastava <nsrivastava@cloudsmith.io>
2026-02-27 07:25:25 +0530

    added pacakage validate call

--- pkg_upload.sh
+++ pkg_upload.sh
@@ -5,18 +5,20 @@
 export NAMESPACE="cloudsmith-org-neeraj" # Replace with your actual namespace
 export REPO_NAME="api-assessment-repo" # You can change the repository name as needed
 export API_KEY=$CLOUDSMITH_API_KEY # Ensure you have set the CLOUDSMITH_API_KEY environment variable with your API key
-export PKG_PATH="npm_pkg.tgz" # Path to your package file, change as needed
+export PKG_PATH="your-package-name-1.0.0.whl" # Path to your package file, change as needed
 
-# Function to upload a package to the repository
-upload_package() {
-  curl -sS \
-  --url "https://api.cloudsmith.io/v1/packages/${NAMESPACE}/${REPO_NAME}/upload/npm/" \
-  -H "X-Api-Key: ${API_KEY}" \
-  -H "accept: application/json" \
-  -F "package_file=@${PKG_PATH}" \
-  -F "npm_dist_tag=latest" \
-  -F "republish=true" | jq
+# Packages Validate Upload Python with status code 200 for successful validation, 400 for failed validation
+curl -sS\
+  --write-out "%{http_code}" \
+  --request POST \
+  --url "https://api.cloudsmith.io/v1/packages/${NAMESPACE}/${REPO_NAME}/validate-upload/python/" \
+  --header 'accept: application/json' \
+  --header 'content-type: application/json' \
+  --header "X-Api-Key: ${API_KEY}" \
+  --data '
+{
+    "package_file": "'"${PKG_PATH}"'",
+    "republish": true,
+    "tags": "python"
 }
-
-echo "#### Uploading a package to '${REPO_NAME}' in namespace: '${NAMESPACE}' ####"
-upload_package
+' | jq
