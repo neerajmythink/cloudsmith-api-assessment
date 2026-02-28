da23fb99eef5828a7ffc77dde87b2347d6c1e0a3
Neeraj.Srivastava <nsrivastava@cloudsmith.io>
2026-02-28 18:57:33 +0530

    improved readability

--- pkg_list.sh
+++ pkg_list.sh
@@ -3,7 +3,7 @@
 # List all packages in a given repository  
 
 export NAMESPACE="cloudsmith-org-neeraj" # Replace with your actual namespace
-export REPO_NAME="assessment-repo" # You can change the repository name as needed
+export REPO_NAME="api-assessment-repo" # You can change the repository name as needed
 export API_KEY=$CLOUDSMITH_API_KEY # Ensure you have set the CLOUDSMITH_API_KEY environment variable with your API key
 
 # Function to list all packages in the repository
@@ -12,7 +12,9 @@ list_packages() {
      --request GET \
      --url "https://api.cloudsmith.io/packages/${NAMESPACE}/${REPO_NAME}/?sort=-date" \
      --header "X-Api-Key: ${API_KEY}" \
-     --header 'accept: application/json' | jq '.[]' | jq '.slug'
+     --header 'accept: application/json' | jq '.[]' | jq -r '.slug + " - " + .slug_perm + " - " + .subtype'
 }
-echo "#### Listing all packages in '${REPO_NAME}' in namespace: '${NAMESPACE}' ####"
+
+echo -e "\033[1;35m# Listing all packages in repository '${REPO_NAME}' in namespace: '${NAMESPACE}' with the following details:\033[0m"
+echo -e "\033[1;36mPkg_Name - slug_perm - Type\033[0m"
 list_packages
