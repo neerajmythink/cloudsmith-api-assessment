7f6a3e0ac5dbd1ce79bb47646c86631cc958f473
Neeraj.Srivastava <nsrivastava@cloudsmith.io>
2026-03-02 12:40:20 +0530

    improved readability of list webhook

--- repo_webhooks_list.sh
+++ repo_webhooks_list.sh
@@ -3,7 +3,7 @@
 # Script to list the webhooks for the given repository.
 export NAMESPACE="cloudsmith-org-neeraj" # Replace with your actual namespace
 export API_KEY=$CLOUDSMITH_API_KEY # Ensure you have set the CLOUDSMITH_API_KEY environment variable with your API key
-export REPO_NAME="assessment-repo" # You can change the repository name as needed
+export REPO_NAME="example_repo_through_cli" # You can change the repository name as needed
 
 # Function to set up a webhook for npm packages
 get_webhook() {
@@ -11,8 +11,10 @@ get_webhook() {
     --request GET \
     --url https://api.cloudsmith.io/webhooks/${NAMESPACE}/${REPO_NAME}/ \
     --header "X-Api-Key: ${API_KEY}" \
-    --header 'accept: application/json' | jq '.[]' | jq '.slug_perm'
+    --header 'accept: application/json' | jq '.[]' | jq '.slug_perm + " - " + .created_at + " - " + .created_by'
 }
 
-echo "#### Getting list of webhook 'slug_prem' for given repo '${REPO_NAME}' in namespace: '${NAMESPACE}' ####"
+# print echo statement to indicate the start of the webhook listing process in pink color
+echo -e "\033[35m#### Getting list of webhook 'slug_perm' for given repo '${REPO_NAME}' in namespace: '${NAMESPACE}' ####\033[0m"
+echo 'slug_perm - created_at - created_by'
 get_webhook
