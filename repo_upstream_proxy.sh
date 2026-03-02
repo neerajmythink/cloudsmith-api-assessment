c455ea7770341331c45905b514ae868857b6d80b
Neeraj.Srivastava <nsrivastava@cloudsmith.io>
2026-03-02 12:46:45 +0530

    changed the repo name

--- repo_upstream_proxy.sh
+++ repo_upstream_proxy.sh
@@ -3,7 +3,7 @@
 # Set up an upstream proxy for the repository created in the previous step. This will allow you to cache and proxy packages from the Python Package Index (PyPI).
 export NAMESPACE="cloudsmith-org-neeraj" # Replace with your actual namespace
 export API_KEY=$CLOUDSMITH_API_KEY # Ensure you have set the CLOUDSMITH_API_KEY environment variable with your API key
-export REPO_NAME="assessment-repo" # You can change the repository name as needed
+export REPO_NAME="example_repo_through_cli" # You can change the repository name as needed
 
 # Function to set up upstream proxy
 set_upstream() {
